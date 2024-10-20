ffc9bbf7ccc245daa426875fdb70b93e6b613bdb
Svein Gonzalez <42025111+sveinerss@users.noreply.github.com>
2024-10-20 15:46:36 -0700

    updated allergenie barcode files

---
+++ svein/allergenie_web/lib/api_service.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+import 'dart:typed_data';
+import 'package:http/http.dart' as http;
+
+
+class ApiService {
+  final String baseUrl;
+
+  ApiService(this.baseUrl);
+
+  Future<String> uploadImage(Uint8List imageData) async {
+    final response = await http.post(
+      Uri.parse('$baseUrl/upload'), 
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: json.encode({'image': base64Encode(imageData)}),
+    );
+
+    if (response.statusCode == 200) {
+      return response.body;
+    } else {
+      throw Exception('Failed to upload image');
+    }
+  }
+}
