3392bce3b7ff9c3f779808d9f5d8b10867c2316b
Phoebe Yueh <130858240+phoebeyueh@users.noreply.github.com>
2024-11-13 22:00:06 -0500

    Update main_rag.dart

---
+++ phoebe/allergenie_web/lib/main_rag.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'rag_screen/home_screen.dart';
+
+void main() {
+  runApp(RecipeGeneratorApp());
+}
+
+class RecipeGeneratorApp extends StatelessWidget {
+  const RecipeGeneratorApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Recipe Generator',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
